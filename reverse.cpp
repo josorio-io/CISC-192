2ca6cb14abda3e08f7ba50bce6f2ca3cf014cac6
josorio-io <146808534+josorio-io@users.noreply.github.com>
2024-08-09 11:04:14 -0700

    Q1

---
+++ reverse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int reverseDigits(int number);
+
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+      qstd::cin >> number;
+    std::cout << "The number with its digits reversed is: " << reverseDigits(number) << std::endl;
+    return 0;
+}
+
+int reverseDigits(int number) {
+    int reversedNumber = 0;
+    while (number > 0) {
+        reversedNumber = (reversedNumber * 10) + (number % 10);
+        number /= 10;
+    }
+    return reversedNumber;
+}
